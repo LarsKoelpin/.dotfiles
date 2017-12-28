6d7423ababf2b33c22182b3f3d5ba289781025cf
Lars Kölpin <lrsklpn@live.de>
2017-12-29 00:36:22 +0100

    Add syslink.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt-get update; \
 sudo apt-get install stow; \
 sh install/nvm.sh; \
-sh install/zsh.sh; \ 
+sh install/zsh.sh; \
 sh install/tools.sh \
-sh install/docker.sh
+sh install/docker.sh \
+sh install/syslinks.sh
