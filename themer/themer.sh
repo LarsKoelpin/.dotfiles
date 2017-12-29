fcf0a2c25eecb0aee21cb8394c915bb95988e28d
Lars Kölpin <lrsklpn@live.de>
2017-12-29 12:03:05 +0100

    Move themer install

--- themer/themer.sh
+++ themer/themer.sh
@@ -19,4 +19,4 @@ create_new_profile() {
 # Create profile
 id=$(create_new_profile THEMER_LKO)
 # Execute themer
-../themer/gen/themer-gnome-terminal/gnome-terminal-dark-install.sh THEMER_LKO
+./gen/themer-gnome-terminal/gnome-terminal-dark-install.sh THEMER_LKO
