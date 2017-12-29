626a8b04a26326498f1d770c46622d43c21eadc3
Lars Kölpin <LarsKoelpin@users.noreply.github.com>
2017-12-29 11:47:13 +0100

    Update themer.sh

--- install/themer.sh
+++ install/themer.sh
@@ -18,3 +18,6 @@ create_new_profile() {
 
 # Create profile
 id=$(create_new_profile THEMER_LKO)
+
+# Execute themer
+sh ../themer/gen/themer-gnome-terminal/gnome-terminal-dark-install.sh THEMER_LKO
